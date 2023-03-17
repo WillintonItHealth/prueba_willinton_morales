a7a5dc7d6e0a8c3b57292213cea2c556b2d2e3c1
WillintonItHealth <114110127+WillintonItHealth@users.noreply.github.com>
2023-03-17 16:59:25 -0500

    Webs de Crud

---
+++ Solucion/prueba_willinton_morales_back/prueba_willinton_morales_front/Front/Pages/EditarPaciente.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Front.Pages
+{
+    public class EditarPacienteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Solucion/prueba_willinton_morales_back/prueba_willinton_morales_front/Front/Pages/EliminarPaciente.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Front.Pages
+{
+    public class EliminarPacienteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
