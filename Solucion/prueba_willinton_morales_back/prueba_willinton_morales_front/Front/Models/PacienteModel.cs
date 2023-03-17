f3a615ff3602397d261c819431a85de2cc657613
WillintonItHealth <114110127+WillintonItHealth@users.noreply.github.com>
2023-03-17 16:20:02 -0500

    Front del proyecto

---
+++ Solucion/prueba_willinton_morales_back/prueba_willinton_morales_front/Front/Models/PacienteModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Front.Models
+{
+    public class PacienteModel
+    {
+        /// <summary>
+        /// Id del tipo de documento. Tipo Entero
+        /// </summary>
+		public int IdTipoDocumento { get; set; }
+        /// <summary>
+        /// Número de documento. Tipo string
+        /// </summary>
+		public string NumeroDocumento { get; set; }
+        /// <summary>
+        /// Nombres del Paciente. Tipo string
+        /// </summary>
+	    public string Nombres { get; set; }
+        /// <summary>
+        /// Apellidos del paciente. Tipo string
+        /// </summary>
+        public string Apellidos { get; set; }
+        /// <summary>
+        /// Correo electrónico del paciente. Tipo string
+        /// </summary>
+        public string Correo { get; set; }
+        /// <summary>
+        /// Número Telefónico del paciente. Tipo string
+        /// </summary>
+        public string Telefono { get; set; }
+        /// <summary>
+        /// Fecha de nacimiento del paciente. Tipo DateTime
+        /// </summary>
+        public DateTime FechaNacimiento { get; set; }
+        /// <summary>
+        /// Estado de la afiliacion. Tipo bool
+        /// </summary>
+        public bool EstadoAfiliacion { get; set; }
+    }
+}
